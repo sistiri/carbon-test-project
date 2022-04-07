faba40bf9ad2ce663a6e59df6bf50d00614ed003
sistiri <sistiri@yahoo.com>
2022-04-07 14:39:50 +0200

    Starting Project

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,21 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+// FROM V11 GITHUB BRANCH:
+
+// import React from 'react';
+// import RepoPage from './content/RepoPage';
+// import { shallow } from 'enzyme';
+
+// describe('React Step 2 Tests', () => {
+//   it('renders without crashing', () => {
+//     shallow(<RepoPage />);
+//   });
+
+//   const wrapper = shallow(<RepoPage />);
+//   it('contains a RepoTable', () => {
+//     expect(wrapper.find('RepoTable').length).toBe(1);
+//   });
+// });
