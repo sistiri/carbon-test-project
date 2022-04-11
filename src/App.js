cc1ad665be0bdbee1106fb7129dfa14ccf0a6679
sistiri <sistiri@yahoo.com>
2022-04-11 15:06:59 +0200

    App Component with Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { Content, Theme } from "@carbon/react";
+import { Routes, Route } from "react-router-dom";
 import "./app.scss";
-import { Button } from 'carbon-components-react';
+import DemoHeader from "./components/DemoHeader";
+import LandingPage from "./content/LandingPage/LandingPage";
+import DemoPage from "./content/DemoPage";
+import AboutPage from "./content/AboutPage";
 
 function App() {
   return (
-    <div>
-      <header>
-      <Button>STARTING PROJECT</Button>
-      </header>
-    </div>
+    <>
+      <Theme theme="g100">
+        <DemoHeader />
+      </Theme>
+      <Theme theme="white">
+        <Content>
+          <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/demo" element={<DemoPage/>} />
+            <Route path="/about" element={<AboutPage/>} />
+          </Routes>
+        </Content>
+      </Theme>
+    </>
   );
 }
 
