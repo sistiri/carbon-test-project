9690c5ddd77e0fac644e206e0a69e83bf0c49acc
sistiri <sistiri@yahoo.com>
2022-04-11 15:04:29 +0200

    LandingPage Component

---
+++ src/content/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return <>
+  <div>LANDING PAGE CONTENT 1</div>
+  <div>LANDING PAGE CONTENT 2</div>
+  <div>LANDING PAGE CONTENT 3</div>
+  <div>LANDING PAGE CONTENT 4</div>
+  <div>LANDING PAGE CONTENT 5</div>
+  </>
+};
+
+export default LandingPage;
