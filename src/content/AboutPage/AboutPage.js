a171280cd6f687922ebfee447bfd6f77a927ba89
sistiri <sistiri@yahoo.com>
2022-04-11 15:04:10 +0200

    AboutPage Component

---
+++ src/content/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <>
+      <div>ABOUT PAGE CONTENT 1</div>
+      <div>ABOUT PAGE CONTENT 2</div>
+      <div>ABOUT PAGE CONTENT 3</div>
+    </>
+  );
+};
+
+export default AboutPage;
