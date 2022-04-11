56ee5b2a3d5af346b7f0c3634eb3619f34e7780d
sistiri <sistiri@yahoo.com>
2022-04-11 15:03:47 +0200

    DemoPage Component

---
+++ src/content/DemoPage/DemoPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DemoPage = () => {
+  return (
+    <>
+      <div>DEMO PAGE CONTENT 1</div>
+      <div>DEMO PAGE CONTENT 2</div>
+      <div>DEMO PAGE CONTENT 3</div>
+    </>
+  );
+};
+
+export default DemoPage;
